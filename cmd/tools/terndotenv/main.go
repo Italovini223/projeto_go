352940a09f8e57664c5a98d6f987c65c02c6ed45
italo vinicius <italovinicius2018@gmail.com>
2024-08-07 01:18:51 -0300

    create schemas in db

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		panic(err)
+	}
+
+	fmt.Println("Command Output: ", out.String())
+}
